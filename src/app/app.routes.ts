0d83836e1731d67bfba1c8b7be66bd6650198638
ThinhPhoenix <laithinh1310@gmail.com>
2026-01-30 09:35:25 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ThinhComponent } from './thinh/thinh';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'thinh', component: ThinhComponent }
+];
---
+++ src/app/thinh/thinh.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-thinh',
+    templateUrl: './thinh.html',
+    styleUrls: ['./thinh.css']
+})
+export class ThinhComponent {
+}
